40e5bf57ba58af239139eecfb107e7dd150a7066
Kate <iseeyou926@gmail.com>
2017-05-16 02:28:12 +0300

    added fields and general methods to 2nd lab

--- BFS.java
+++ BFS.java
@@ -1,9 +1,39 @@
+
 /**
  * Created by Kate on 16.05.2017.
  */
 
 
 public class BFS {
+    private String label;
+    private boolean isVisited;
+    public BFS (String label){
+        this.label = label;
+        isVisited = false;
+    }
 
+    private final int VERTEX_MAX = 100;
+    private BFS[] vertexList; //асив для збереження вершин
+    private int vertexCount;
+    private  int [][] matrix;
 
+    public void graph(){
+        matrix = new int[VERTEX_MAX][VERTEX_MAX];
+        for (int i=0; i<VERTEX_MAX; i++)
+            for (int j=0; j<VERTEX_MAX; j++)
+                matrix[i][j]=0;
+        vertexCount = 0;
+        vertexList = new BFS[VERTEX_MAX];
+    }
+
+    public void addVertex (String label){
+        vertexList[vertexCount++] = new BFS(label);
+    }
+
+    public void addEdge (int begin, int end){
+        matrix[begin][end] = 1;
+        matrix[end][begin] = 0;
+    }
+
+    //JQueue queue = new JQueue(100);
 }
