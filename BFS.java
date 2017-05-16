fc12eb6bc1761ec673bd3217ee77ac2ba30f3259
Kate <iseeyou926@gmail.com>
2017-05-17 02:18:27 +0300

    some new

--- BFS.java
+++ BFS.java
@@ -1,4 +1,5 @@
 
+
 /**
  * Created by Kate on 16.05.2017.
  */
@@ -10,6 +11,22 @@ public class BFS {
     public BFS (String label){
         this.label = label;
         isVisited = false;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public boolean isVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(boolean isVisited) {
+        this.isVisited = isVisited;
     }
 
     private final int VERTEX_MAX = 100;
@@ -35,5 +52,42 @@ public class BFS {
         matrix[end][begin] = 0;
     }
 
-    //JQueue queue = new JQueue(100);
+    Queue queue = new Queue(100);
+    //обход в ширину
+    void bfs(int v, int f)
+    {
+        vertexList[v].isVisited = true;
+        queue.insert(v);
+        int vertex;
+
+        //выведем вершину, с которой начинается обход, на экран
+        System.out.println(vertexList[v].getLabel());
+        //for (int i=v; i<=f; i++){
+        while (queue.getRear()==f)//пока очередь не опустеет
+        {
+            int current = queue.pop();
+            while ((vertex = getSuccessor(current)) != -1) {
+                vertexList[vertex].isVisited = true;
+                queue.insert(vertex);
+                //вывод вершины на экран
+                System.out.println(vertexList[vertex].getLabel());
+            }
+
+        }
+    //}
+
+        //сброс флагов
+        for(int j = 0; j < vertexCount; j++)
+            vertexList[j].isVisited = false;
+    }
+
+    //метод возвращает непосещенную вершину, смежную по отношению к v
+    int getSuccessor (int v)
+    {
+        for(int j = 0; j < vertexCount; j++)
+            if(matrix[v][j] == 1 && vertexList[j].isVisited == false)
+                return j; //возвращает первую найденную вершину
+        return -1; //таких вершин нет
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        DistanceMatrix distanceMatrix = new DistanceMatrix();
-        distanceMatrix.findDistanceMatrix();
+        //DistanceMatrix distanceMatrix = new DistanceMatrix();
+        //distanceMatrix.findDistanceMatrix();
+
+        BFS method = new BFS("test");
+        method.graph();
+        method.addVertex("Che");//0
+        method.addVertex("Kyiv");//1
+        method.addVertex("Lviv");//2
+        method.addVertex("Che2");//3
+        method.addVertex("Kyiv2");//4
+        method.addVertex("Lviv2");//5
+        method.addEdge(0, 1);
+        method.addEdge(1, 2);
+        method.addEdge(2, 3);
+        method.addEdge(0, 3);
+        method.addEdge(3, 4);
+
+        method.bfs(0, 3);
 
     }
 }
